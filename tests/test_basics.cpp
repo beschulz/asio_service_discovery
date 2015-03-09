d273a1814b92639d347ffec35d9c18ac4307c524
Benjamin Schulz <beschulz@betabugs.de>
2015-03-09 16:25:58 +0100

    Added basic test to check for buffer overruns.

--- tests/test_basics.cpp
+++ tests/test_basics.cpp
@@ -153,4 +153,49 @@ BOOST_AUTO_TEST_SUITE( basic_tests )
         BOOST_CHECK(did_discover_service);
     }
 
+    BOOST_AUTO_TEST_CASE( test_overflow )
+    {
+        boost::asio::io_service io_service;
+
+        std::string ridiculously_long_service_name;
+        const char hex_chars[] = "0123456789abcdef";
+
+        for(int i=0; i!=1024*8; ++i)
+        {
+            ridiculously_long_service_name.push_back(hex_chars[i % sizeof(hex_chars)]);
+        }
+
+        service_announcer announcer(io_service, ridiculously_long_service_name, 1337);
+
+        bool did_discover_service = false;
+        service_discoverer discoverer(io_service, ridiculously_long_service_name,
+                [&io_service, &did_discover_service, &ridiculously_long_service_name]
+                        (const service_discoverer::services& services)
+                {
+                    BOOST_CHECK(!services.empty());
+
+                    for(const auto& service : services)
+                    {
+                        BOOST_CHECK_EQUAL(service.service_name, ridiculously_long_service_name);
+                        BOOST_CHECK_EQUAL(service.computer_name, boost::asio::ip::host_name());
+                        BOOST_CHECK_EQUAL(service.endpoint.port(), 1337);
+                    }
+
+                    did_discover_service = !services.empty();
+                });
+
+        boost::asio::deadline_timer timer(io_service);
+        timer.expires_from_now(boost::posix_time::seconds(2));
+
+        timer.async_wait(
+                [&io_service, &did_discover_service]
+                        (const boost::system::error_code& error)
+                {
+                    BOOST_CHECK(did_discover_service);
+                    io_service.stop();
+                });
+
+        io_service.run();
+    }
+
 BOOST_AUTO_TEST_SUITE_END()
