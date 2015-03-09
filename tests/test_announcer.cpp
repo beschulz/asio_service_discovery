b7c11c86b03f0af6ec619c65cc7725cd746488fe
Benjamin Schulz <beschulz@betabugs.de>
2015-03-09 18:06:08 +0100

    added a simple test announcer example

---
+++ tests/test_announcer.cpp
@@ -0,0 +1,9 @@
+#include <betabugs/networking/service_announcer.hpp>
+
+int main(int argc, const char * const argv[])
+{
+    boost::asio::io_service io_service;
+    betabugs::networking::service_announcer announcer(io_service, "my_service", 1337);
+    io_service.run();
+    return 0;
+}
