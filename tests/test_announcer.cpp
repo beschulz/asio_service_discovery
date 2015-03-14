f6be8aeeb188b15535c52255b2685ff81f33f5ee
Benjamin Schulz <beschulz@betabugs.de>
2015-03-15 00:48:54 +0100

    fixed warnings code style

--- tests/test_announcer.cpp
+++ tests/test_announcer.cpp
@@ -1,9 +1,12 @@
 #include <betabugs/networking/service_announcer.hpp>
 
-int main(int argc, const char * const argv[])
+int main(int argc, const char* const argv[])
 {
-    boost::asio::io_service io_service;
-    betabugs::networking::service_announcer announcer(io_service, "my_service", 1337);
-    io_service.run();
-    return 0;
+	(void) argc;
+	(void) argv;
+
+	boost::asio::io_service io_service;
+	betabugs::networking::service_announcer announcer(io_service, "my_service", 1337);
+	io_service.run();
+	return 0;
 }
