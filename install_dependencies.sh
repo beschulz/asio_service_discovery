321ebdfc5c747399e30fffc06386d323626a2c07
Benjamin Schulz <beschulz@betabugs.de>
2015-03-15 02:07:34 +0100

    ci

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -10,9 +10,6 @@ then
 else
     #install a newer cmake since at this time Travis only has version 2.8.7
     sudo add-apt-repository ppa:kalakris/cmake -y
-    sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
     sudo apt-get update -qq
     sudo apt-get install -qq libboost-dev cmake
-    if [ "$CXX" = "g++" ]; then sudo apt-get install -qq g++-4.8; fi
-    if [ "$CXX" = "g++" ]; then export CXX="g++-4.8" CC="gcc-4.8"; fi
 fi
