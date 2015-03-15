b18150f26fd7a32c8f63d14949e5d994684c2c86
Benjamin Schulz <beschulz@betabugs.de>
2015-03-15 01:54:30 +0100

    travis: gcc-4.8

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -10,7 +10,10 @@ then
     brew install boost
 else
     #install a newer cmake since at this time Travis only has version 2.8.7
-    echo "yes" | sudo add-apt-repository ppa:kalakris/cmake
+    sudo add-apt-repository ppa:kalakris/cmake -y
+    sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
     sudo apt-get update -qq
     sudo apt-get install -qq libboost-dev cmake
+    if [ "$CXX" = "g++" ]; then sudo apt-get install -qq g++-4.8; fi
+    if [ "$CXX" = "g++" ]; then export CXX="g++-4.8" CC="gcc-4.8"; fi
 fi
